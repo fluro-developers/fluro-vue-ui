834bc3220a5070a73d4447daff20a4b705594742
Timothy Minahan <timothy@minahan.net>
2020-01-29 12:41:24 +1030

    FLURO-253 Allow the injection of tokens into the Fluro WYSIWYG editor.

---
+++ src/components/form/tiptap/token.js
@@ -0,0 +1,40 @@
+import { Node } from 'tiptap'
+
+export default class Token extends Node {
+  get name() {
+    return 'token'
+  }
+
+  get schema() {
+    return {
+      attrs: {
+      },
+      inline: true,
+      selectable: false,
+      content: 'inline*',
+      draggable: false,
+      isText: true,
+      toDOM: node => node.attrs,
+    }
+  }
+
+  commands({ type, schema }) {
+    return attrs => (state, dispatch) => {
+      const { $from } = state.selection
+      const { pos } = $from.pos
+      dispatch(state.tr.insertText(`{{${attrs}}}`, pos))
+    }
+    
+  }
+
+  // keys({ type }) {
+  //   console.log("KEYS CALLED", "This", this, "Type", type)
+  //   return this.options.tokens.reduce((items, key) => ({
+  //     ...items,
+  //   }), {})
+  // }
+
+
+
+
+}
