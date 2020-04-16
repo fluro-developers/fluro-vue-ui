d1e29bf3860987c563ba08b87b0641b442fe3574
Timothy Minahan <timothy@minahan.net>
2020-04-16 14:56:00 +0930

    FLURO-288 Code Cleanup

--- src/components/form/tiptap/image.js
+++ src/components/form/tiptap/image.js
@@ -54,50 +54,5 @@ export default class Image extends Node {
         }
       }
 
-    // get plugins() {
-    //     return [
-    //         new Plugin({
-    //             props: {
-    //                 handleDOMEvents: {
-    //                     drop(view, event) {
-    //                         const hasFiles = event.dataTransfer &&
-    //                             event.dataTransfer.files &&
-    //                             event.dataTransfer.files.length
-
-    //                         if (!hasFiles) {
-    //                             return
-    //                         }
-
-    //                         const images = Array
-    //                             .from(event.dataTransfer.files)
-    //                             .filter(file => (/image/i).test(file.type))
-
-    //                         if (images.length === 0) {
-    //                             return
-    //                         }
-
-    //                         event.preventDefault()
-
-    //                         const { schema } = view.state
-    //                         const coordinates = view.posAtCoords({ left: event.clientX, top: event.clientY })
-
-    //                         images.forEach(image => {
-    //                             const reader = new FileReader()
-
-    //                             reader.onload = readerEvent => {
-    //                                 const node = schema.nodes.image.create({
-    //                                     src: readerEvent.target.result,
-    //                                 })
-    //                                 const transaction = view.state.tr.insert(coordinates.pos, node)
-    //                                 view.dispatch(transaction)
-    //                             }
-    //                             reader.readAsDataURL(image)
-    //                         })
-    //                     },
-    //                 },
-    //             },
-    //         }),
-    //     ]
-    // }
 
 }
