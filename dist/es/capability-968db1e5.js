a45046e8aeac137a1588be86b3c5378be67755b1
jamessandison <james.sandison@chbc.org.au>
2020-05-22 19:02:33 +1000

    CSS extract

---
+++ dist/es/capability-968db1e5.js
@@ -0,0 +1,98 @@
+import { bd as __vue_normalize__ } from './index-a26484f6.js';
+import 'vue';
+import 'vuedraggable';
+import 'vue-color';
+import 'signature_pad';
+import 'fluro';
+import 'vuex-map-fields';
+import 'async';
+import 'tippy.js';
+import 'tiptap';
+import 'tiptap-commands';
+import 'tiptap-extensions';
+import { F as FluroContentEditMixin } from './FluroContentEditMixin-f94812ea.js';
+
+//
+
+var script = {
+  mixins: [FluroContentEditMixin],
+  components: {},
+  methods: {
+    modelUpdated() {
+      this.update(this.model);
+    }
+
+  },
+
+  created() {
+  },
+
+  asyncComputed: {},
+  computed: {
+    showOutline() {
+      return false; //true; //false;//true;//false;//true;
+    },
+
+    fieldsOutput() {
+      var array = []; ///////////////////////////////////
+
+      addField('title', {
+        title: 'Title',
+        minimum: 1,
+        maximum: 1,
+        type: 'string'
+      });
+
+      function addField(key, details) {
+        details.key = key;
+        array.push(details);
+      }
+
+      return array;
+    }
+
+  },
+
+  data() {
+    return {};
+  }
+
+};
+
+/* script */
+const __vue_script__ = script;
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('flex-column',[(_vm.loading)?[_c('fluro-page-preloader',{attrs:{"contain":""}})]:_c('tabset',{attrs:{"justified":true,"vertical":true}},[_c('tab',[_c('flex-column-body',{staticStyle:{"background":"#fafafa"}},[_c('v-container',[_c('constrain',{attrs:{"sm":""}},[_c('div',{staticClass:"grid-list-xl"},[_c('fluro-content-form-field',{attrs:{"form-fields":_vm.formFields,"outline":_vm.showOutline,"options":_vm.options,"field":_vm.fieldHash.title},on:{"input":_vm.update},model:{value:(_vm.model),callback:function ($$v) {_vm.model=$$v;},expression:"model"}}),_vm._v(" "),(_vm.definition && _vm.definition.fields && _vm.definition.fields.length)?_c('div',[_c('fluro-content-form',{attrs:{"options":_vm.options,"fields":_vm.definition.fields},model:{value:(_vm.model.data),callback:function ($$v) {_vm.$set(_vm.model, "data", $$v);},expression:"model.data"}})],1):_vm._e()],1)])],1)],1)],1)],1)],2)};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  const __vue_inject_styles__ = undefined;
+  /* scoped */
+  const __vue_scope_id__ = "data-v-3daf1968";
+  /* module identifier */
+  const __vue_module_identifier__ = undefined;
+  /* functional template */
+  const __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
+  /* style inject shadow dom */
+  
+
+  
+  const __vue_component__ = __vue_normalize__(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    false,
+    undefined,
+    undefined,
+    undefined
+  );
+
+export default __vue_component__;
+//# sourceMappingURL=capability-968db1e5.js.map
---
+++ dist/es/post-dabd66b4.js
@@ -0,0 +1,167 @@
+import { bd as __vue_normalize__ } from './index-a26484f6.js';
+import 'vue';
+import 'vuedraggable';
+import 'vue-color';
+import 'signature_pad';
+import 'fluro';
+import 'vuex-map-fields';
+import 'async';
+import 'tippy.js';
+import 'tiptap';
+import 'tiptap-commands';
+import 'tiptap-extensions';
+import { F as FluroContentEditMixin } from './FluroContentEditMixin-f94812ea.js';
+
+//
+
+var script = {
+  mixins: [FluroContentEditMixin],
+  components: {},
+  methods: {
+    modelUpdated() {
+      this.update(this.model);
+    }
+
+  },
+
+  created() {
+  },
+
+  asyncComputed: {},
+  computed: {
+    fieldsOutput() {
+      var array = []; ///////////////////////////////////
+      //We need to add more smarts here so we can
+      //only attach to the correct things
+
+      addField('parent', {
+        title: 'Linked To',
+        minimum: 1,
+        maximum: 1,
+        type: 'reference' // params: {
+        // restrictType: 'event',
+        // },
+
+      }); ///////////////////////////////////
+
+      function addField(key, details) {
+        details.key = key;
+        array.push(details);
+      }
+
+      return array;
+    },
+
+    showOutline() {
+      return false; //true; //false;//true;//false;//true;
+    } // fieldsOutput() {
+    //     var self = this;
+    //     var array = [];
+    //     ///////////////////////////////////
+    //     addField('event', {
+    //         title: 'Event',
+    //         minimum: 1,
+    //         maximum: 1,
+    //         type: 'reference',
+    //         params: {
+    //             restrictType: 'event',
+    //         },
+    //     })
+    //     addField('contact', {
+    //         title: 'Contact',
+    //         minimum: 1,
+    //         maximum: 1,
+    //         type: 'reference',
+    //         description: 'The person who checked was checked in',
+    //         params: {
+    //             restrictType: 'contact',
+    //         },
+    //     })
+    //     addField('checkedInBy', {
+    //         title: 'Checked In By',
+    //         minimum: 1,
+    //         maximum: 1,
+    //         type: 'reference',
+    //         description: 'The person who checked this contact in',
+    //         params: {
+    //             restrictType: 'contact',
+    //         },
+    //     })
+    //     addField('pinNumber', {
+    //         title: 'Pin Number',
+    //         minimum: 0,
+    //         maximum: 1,
+    //         type: 'password',
+    //         description: 'The Pin Number used when checking in (If any)',
+    //     })
+    //     // addField('firstLine', {
+    //     //     title: 'Description',
+    //     //     minimum: 0,
+    //     //     maximum: 1,
+    //     //     type: 'string',
+    //     //     placeholder: 'Eg. A list of my favorite people',
+    //     // })
+    //     // // console.log('TYPE OPTIONS', self.typeOptions);
+    //     // addField('filterType', {
+    //     //     title: 'Type',
+    //     //     minimum: 0,
+    //     //     maximum: 1,
+    //     //     type: 'string',
+    //     //     directive: 'select',
+    //     //     description: 'The type of item you want to return from this query',
+    //     //     options: self.typeOptions,
+    //     // })
+    //     ///////////////////////////////////
+    //     function addField(key, details) {
+    //         details.key = key;
+    //         array.push(details)
+    //     }
+    //     return array;
+    // },
+
+
+  },
+
+  data() {
+    return {};
+  }
+
+};
+
+/* script */
+const __vue_script__ = script;
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('flex-column',[(_vm.loading)?[_c('fluro-page-preloader',{attrs:{"contain":""}})]:_c('tabset',{attrs:{"justified":true,"vertical":true}},[(_vm.definition && _vm.definition.fields && _vm.definition.fields.length)?_c('tab',{attrs:{"heading":((_vm.definition.title) + " Information")}},[_c('flex-column-body',{staticStyle:{"background":"#fafafa"}},[_c('v-container',[_c('constrain',{attrs:{"sm":""}},[(!_vm.model._id)?[_c('fluro-content-form-field',{attrs:{"form-fields":_vm.formFields,"outline":_vm.showOutline,"options":_vm.options,"field":_vm.fieldHash.parent},on:{"input":_vm.update},model:{value:(_vm.model),callback:function ($$v) {_vm.model=$$v;},expression:"model"}})]:[_c('v-input',{staticClass:"no-flex",attrs:{"label":"Linked to"}},[_c('list-group',[_c('list-group-item',{attrs:{"item":_vm.model.parent},nativeOn:{"click":function($event){return _vm.$fluro.global.edit(_vm.model.parent, true)}},scopedSlots:_vm._u([{key:"right",fn:function(){return [_c('v-btn',{staticClass:"ma-0",attrs:{"icon":"","small":"","flat":""},on:{"click":function($event){$event.stopPropagation();$event.preventDefault();return _vm.$actions.open([_vm.model.parent])}}},[_c('fluro-icon',{attrs:{"icon":"ellipsis-h"}})],1)]},proxy:true}],null,false,1779438708)})],1)],1)],_vm._v(" "),_c('fluro-content-form',{attrs:{"options":_vm.options,"fields":_vm.definition.fields},model:{value:(_vm.model.data),callback:function ($$v) {_vm.$set(_vm.model, "data", $$v);},expression:"model.data"}})],2)],1)],1)],1):_vm._e()],1)],2)};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  const __vue_inject_styles__ = undefined;
+  /* scoped */
+  const __vue_scope_id__ = "data-v-8dd26372";
+  /* module identifier */
+  const __vue_module_identifier__ = undefined;
+  /* functional template */
+  const __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
+  /* style inject shadow dom */
+  
+
+  
+  const __vue_component__ = __vue_normalize__(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    false,
+    undefined,
+    undefined,
+    undefined
+  );
+
+export default __vue_component__;
+//# sourceMappingURL=post-dabd66b4.js.map
